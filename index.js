d92364a17c154826f37e05f354ff8c552ec1c144
Isaiah <zaymgmt707@gmail.com>
2024-06-27 15:21:21 -0700

    updates index js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function writeCards(names) {
-    let messages = [];
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+  messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
 
-    for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    }
-
-    return messages;
+  return messages
 }
+
 function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-        console.log(i)
-    }
+  for (let i = number; i >= 0; i--) {
+  console.log(i)
+  }
 }
+
 countDown(9)
